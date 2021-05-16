c46f9a1c24bad04642d34cacd2c6ea2cd2c41df5
Sivadass N <nsivadass@gmail.com>
2021-05-16 20:54:10 +0530

    Removed preview window

--- script.js
+++ script.js
@@ -3,12 +3,7 @@ const runButton = document.querySelector("button.run");
 function showPreview() {
   var jsCode = document.getElementById("jsCode").value;
   var contentWindow = document.getElementById("output").contentWindow;
-  var result = contentWindow.eval(jsCode);
-  console.log(result);
-  const frame = contentWindow.document;
-  frame.open();
-  frame.write(result);
-  frame.close();
+  contentWindow.eval(jsCode);
 }
 
 runButton.addEventListener("click", showPreview);
