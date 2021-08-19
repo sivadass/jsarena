b5179150373bd39d4171f8f153c7619f8cf70da5
Sivadass N <nsivadass@gmail.com>
2021-08-19 21:00:21 +0530

    Fixed Google Analytics

---
+++ web/src/scripts/utils/initializeGA.js
@@ -0,0 +1,8 @@
+window["ga-disable-G-7R092VY3DE"] =
+  process.env.DISABLE_GOOGLE_ANALYTICS === "true";
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-7R092VY3DE");
